7a5da5dc1be300f24d2fe1ad905052cb8187c949
Abdul Wahab Khan <awkmeta@gmail.com>
2025-06-29 08:51:59 +0300

    Add Dev Sequence generator (Python)
    Initial commit of the Python script that generates the filtered 9n+6 Dev Sequence

---
+++ dev_sequence.py
@@ -0,0 +1,14 @@
+def digit_sum(n):
+    return sum(int(d) for d in str(n))
+
+def wahab_seq(n_terms):
+    seq, n = [], 0
+    last = None
+    while len(seq) < n_terms:
+        val = 9 * n + 6
+        if digit_sum(val) == 6:
+            if last is None or digit_sum(val - last) == 9:
+                seq.append(val)
+                last = val
+        n += 1
+    return seq
